6f717ee5ac3d1c8f1f16fff403866d037e684cb3
Nicholas Trambitas <47403010+ntrambitas12@users.noreply.github.com>
2022-12-13 19:21:04 -0500

    Add files via upload

---
+++ biotechhomes-dev/solar-panel-energy.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include './shared/header.php';?>
+
+<body>
+
+ 
+  <!--/ Intro Single star /-->
+  <section class="intro-single">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 col-lg-8">
+          <div class="title-single-box">
+            <h1 class="title-single">Solar Panel Energy</h1>
+          </div>
+        </div>
+        <div class="col-md-12 col-lg-4">
+          <nav aria-label="breadcrumb" class="breadcrumb-box d-flex justify-content-lg-end">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item">
+                <a href="home.php">Home</a>
+              </li>
+              <li class="breadcrumb-item active" aria-current="page">
+                Solar Panel Energy
+              </li>
+            </ol>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--/ Intro Single End /-->
+
+  <!--/ News Single Star /-->
+  <section class="news-single nav-arrow-b">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="news-img-box">
+            <img src="img/solar-power.jpg" alt="" class="img-fluid">
+          </div>
+        </div>
+        <div class="col-md-10 offset-md-1 col-lg-8 offset-lg-2">
+          <div class="post-information">
+          </div>
+          <div class="post-content color-text-a">
+          <h1>Solar Panel Energy </h1>
+            <p>
+                 Solar panel energy is a solution to obtaining clean, renewable power. Every second,
+              173,000 terawatts of solar energy strike the Earth. This is more than ten times our current 
+              energy consumption. Currently, the United States is the second-largest solar market in 
+              the world. The most common places to find solar panels are rooftops, spacecraft, and handheld 
+              calculators.
+            </p>
+
+            <h2> How it works </h2>
+
+            <p>
+            The sun is constantly giving out particles called photons. The solar panel turns the photons into electrons which can be used as DC energy. Then, the DC power goes through an inverter which changes it into AC power. AC power is then able to go straight into your computers, televisions, and other electronic devices.
+Because the sun isn’t out in the night, a net energy meter is used. The net energy meter is used to track all of the power being used and to save power for later when it is either cloudy or the sun isn’t out.
+For the most part, anyone could install their own solar panels on their home, but to refrain from injury it is best to contact a personal contractor.
+
+            </p>
+            
+            <h2>Why You Should Switch</h2>
+            <ul>
+                <li>The Environment:</li>
+                <p> Solar panels don’t produce any air pollution, water pollution, or any 
+                greenhouse gases. Using them can help lower the harmful effects brought by other
+                 energy sources.</p>
+                 <li>Cost:</li>
+                 <p> The average electric bill is $0.13 per kilowatt-hour. On average in the US,
+                  each household uses 10,399 kilowatt-hours a year. This gives the costly bill of 
+                  $1351 every year (or $112 a month) On average, it costs around $14,870 to install 
+                  solar panels on your home. So, in the long run, you’ll save thousands of dollars a
+                   year.</p>
+            </ul>
+
+            <h2> Fun Fact: The First Solar Panel </h2>
+            <p>
+            The first solar panels were built in 1945 by Bell Laboratories. 
+            This was a breakthrough to “a new era.” Eventually, during the 1950’s, 
+            the technology was adopted into the spacecraft: “The Vanguard 1.” 
+            The spacecraft used the panels to keep its power while remaining its orbit around the Earth.
+            </p>
+
+
+          </div>
+          <div class="post-footer">
+            <div class="post-share">
+              <span>Share: </span>
+              <ul class="list-inline socials">
+                <li class="list-inline-item">
+                  <a href="https://www.facebook.com/biotech.homes.5" target="_blank">
+                    <i class="fa fa-facebook" aria-hidden="true"></i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="https://www.twitter.com/BiotechHomes" target="_blank">
+                    <i class="fa fa-twitter" aria-hidden="true"></i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="https://www.instagram.com/biotechhomes" target="_blank">
+                    <i class="fa fa-instagram" aria-hidden="true"></i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="https://www.pinterest.com/biotechhomes" target="_blank">
+                    <i class="fa fa-pinterest-p" aria-hidden="true"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--/ News Single End /-->
+
+  <?php include './shared/footer.php';?>
+
+</body>
+</html>
